8ed743825e65096a2bc72cf47bf3dd7a0865cbc1
Hiroharu Tanaka <hiroharu8864@gol.com>
2014-05-27 16:16:57 +0900

    modify attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:apache_tomcat] = {
-  :source => 'http://ftp.kddilabs.jp/infosystems/apache/tomcat/tomcat-7/v7.0.53/bin/apache-tomcat-7.0.53.tar.gz',
+  :source => 'http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/tomcat/tomcat-7/v7.0.54/bin/apache-tomcat-7.0.54.tar.gz',
 }
 default[:gitbucket] = {
   :source => 'https://github.com/takezoe/gitbucket/releases/download/1.13/gitbucket.war',
